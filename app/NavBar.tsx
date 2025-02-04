01d2703ec8c764fb35f16f44d40419ba9661eabd
saminthapa <thapasamin4@gmail.com>
2025-02-04 15:48:31 +0545

    style active link in nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,30 @@
+'use client'
+
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { VscDebug } from "react-icons/vsc";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
     const links = [
         { label: 'Dashboard', herf: '/'},
         { label: 'Issues', herf: '/issues'}
     ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <nav className='flex space-x-6 border-b mb-5 px-5  h-14 items-center'>
         <Link href='/'><VscDebug /></Link>
         <ul className='flex space-x-6'>
             {links.map(link =>
             <Link 
             key={link.herf} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+            className={classNames({
+              'text-zinc-900': link.herf === currentPath,
+              'text-zinc-500': link.herf !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })} 
             href={link.herf}>{link.label}</Link>)}
             
         </ul>
